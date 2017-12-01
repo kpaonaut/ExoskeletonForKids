464f50c916fa053e54b53fde655cf896e2f5bdfd
ArayCHN <1172094131@qq.com>
2017-11-30 18:00:05 -0800

    completed 1 milis function

--- STM32Project/main.cpp
+++ STM32Project/main.cpp
@@ -10,13 +10,18 @@ DigitalOut led1(LED1); // blink and debug
 DigitalOut led2(LED2);
 Serial pc(USBTX,USBRX,115200);
 static float hipAngle = 0;
+bool stop = 0;
+TrajectoryGenerator generator;
+
 void periodic_function()
 {
     static int counter = 0;
     counter++;
+    
+    float hipAngle = generator.generateTrajectory();
+    pc.printf("%7.2f\n", hipAngle);
     if((counter%1000)==0) {
         led2=!led2;
-        pc.printf("Hip angle:%f\n", hipAngle);
     }
 }
 
@@ -25,22 +30,19 @@ int main()
 {
     pc.printf("main\n\r");
     
-    hipAngle = 0;
     Ticker control_loop;
     control_loop.attach(&periodic_function, 0.001);
-    bool stop = 0;
-    TrajectoryGenerator generator;
+    
     while (true) {
         if (pc.readable()) {
             char c = pc.getc();
             pc.printf("User inserted: %c\n\r",c);
             led1 = !led1;
             stop = !stop;
+            if (stop)
+                generator.stopTrigger();
+            else 
+                generator.resumeTrigger();
         }
-        if (stop)
-            generator.stopTrigger();
-        else 
-            generator.resumeTrigger();
-        hipAngle = generator.generateTrajectory();
     }
 }
--- STM32Project/trajectory_generator.h
+++ STM32Project/trajectory_generator.h
@@ -2,12 +2,10 @@
 #include "hip.h"
 #include "knee.h"
 #include "spline.h"
-#include <iostream> // debug
 #include <cstdio>
 #include <cstdlib>
 #include <vector>
-// #include "mbed.h"
-// Serial pc2(USBTX,USBRX,115200);
+
 class TrajectoryGenerator{
 private:
     float t, walking_angle, sample_time, halfTotalTime;
@@ -50,33 +48,7 @@ public:
 
         hip_trajectory_generator_l.init(); // set coordinates for interpolation
         knee_trajectory_generator_l.init();
-/*
-        // right Hip
-        hip_trajectory_generator_r = StepHipTraj();  // right leg
 
-        hip_trajectory_generator_r.reset();
-        hip_trajectory_generator_r.set_max_hip_flexion( _max_hip_flexion_ ); // degrees, default 36
-        hip_trajectory_generator_r.set_max_hip_flexion_time( _max_hip_flexion_time_portion_ * halfTotalTime/sample_time);       // in ms
-        hip_trajectory_generator_r.set_step_time( halfTotalTime / sample_time ); // in ms, integer. 5000 here.
-        hip_trajectory_generator_r.set_walking_angle( walking_angle );                 // offset, can vary
-        hip_trajectory_generator_r.set_swing_start( _hip_swing_start_ ); // default -6.934
-        hip_trajectory_generator_r.set_step_range( _step_range_ ); // step length in meter. example: 0.78m legLen 1.1m -> theta 41.8
-        hip_trajectory_generator_r.set_leg_length( _leg_length_ );
-
-        // right Knee
-        knee_trajectory_generator_r = StepKneeTraj();  // new object
-
-        knee_trajectory_generator_r.reset();
-        knee_trajectory_generator_r.set_max_hip_flexion_time( _max_hip_flexion_time_portion_*halfTotalTime/sample_time );       // in ms
-        knee_trajectory_generator_r.set_step_time( halfTotalTime/sample_time ); // in ms, integer. 5000 here.
-        knee_trajectory_generator_r.set_max_knee_flexion( _max_knee_flexion_ ); // in degrees
-        knee_trajectory_generator_r.set_second_knee_flexion( _second_knee_flexion_ );
-        knee_trajectory_generator_r.set_min_knee_flexion( _min_knee_flexion_ );
-        knee_trajectory_generator_r.set_walking_angle( walking_angle );
-
-        hip_trajectory_generator_r.init() // set coordinates for interpolation
-        knee_trajectory_generator_r.init()
-*/
         doneHip = 0;
         doneKnee = 0;
         stopTriggered = 0;
@@ -95,18 +67,6 @@ public:
     void carryOut(){ // carry out calculation
         doneHip = hip_trajectory_generator_l.Increment(&hip_angle);
         doneKnee = knee_trajectory_generator_l.Increment(&knee_angle); // generate traj in next time step
-        /* carry out the value for the other leg. No need in STM32!
-        if ((rightStarted == 1) || (hip_trajectory_generator_l.get_time() > halfTotalTime))
-        {
-            doneHip= hip_trajectory_generator_r.Increment(&hip_angle); // hip angle will be changed!
-            doneKnee = knee_trajectory_generator_r.Increment(); // generate traj in next time step
-            rightStarted = 1;
-        }
-        else
-        {
-            hip_value_r = hip_trajectory_generator_r.get_initial_pos();
-            knee_value_r = knee_trajectory_generator_r.get_initial_pos();
-        }*/
     }
 
     float generateTrajectory(){
@@ -116,11 +76,7 @@ public:
             else;
         else // stop signal not triggered
             carryOut();
-        
-        // angles = [walking_angle, hip_value_l - walking_angle, knee_value_l,
-        //                    hip_value_r - walking_angle, knee_value_r];
-        // print hip_trajectory_generator_l.get_time(), hip_trajectory_generator_r.get_time()
-        //pc.printf("%f!!!\n", hip_angle);
+
         return hip_angle; // only return hip! When is knee needed?
     }
 
